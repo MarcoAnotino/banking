5d74a7eb6be59c820fd2e8cfed697eb5d1ec0877
MarcoAnotino <marcoanotino@Marcos-MacBook-Pro.local>
2025-01-14 02:44:46 -0600

    Ya se ven las transacciones y la dona, y se puede agregar otra cuenta bancaria

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,62 +1,57 @@
 import HeaderBox from '@/components/HeaderBox'
+import RecentTransactions from '@/components/RecentTransactions';
 import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
-import { getLoggedInUser } from '@/lib/actions/user.actions'
-
+import { getLoggedInUser } from '@/lib/actions/user.actions';
 
 const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
-    const loggedIn = await getLoggedInUser();
-    const accounts = await getAccounts({ 
-      userId: loggedIn.$id 
-    })
+  const currentPage = Number(page as string) || 1;
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ 
+    userId: loggedIn.$id 
+  })
 
-    if(!accounts) return;
+  if(!accounts) return;
+  
+  const accountsData = accounts?.data;
+  const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
 
-    const accountsData = accounts?.data;
-    const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
-    
-    if (!appwriteItemId) {
-        console.error("appwriteItemId is not defined");
-        return;
-    }
+  const account = await getAccount({ appwriteItemId })
 
-    const account = await getAccount({ appwriteItemId })
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
 
-    console.log(
-        accountsData,
-        account
-    )
-    
-    return (
-        <section className='home'>
-            <div className='home-content'>
-                <header className='home-header'>
-                    <HeaderBox
-                        type="greeting"
-                        title="Welcome"
-                        user={loggedIn?.firstName || 'Guest'}
-                        subtext="Acces and manage oyur account and transactions efficiently."
-                    />
+          <TotalBalanceBox 
+            accounts={accountsData}
+            totalBanks={accounts?.totalBanks}
+            totalCurrentBalance={accounts?.totalCurrentBalance}
+          />
+        </header>
 
-                    <TotalBalanceBox 
-                        accounts={[accountsData]}
-                        totalBanks={accounts?.totalBanks}
-                        totalCurrentBalance={accounts?.totalCurrentBalance}
-                    />
-                </header>
+        <RecentTransactions 
+          accounts={accountsData}
+          transactions={account?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        />
+      </div>
 
-                RECENT TRANSACTIONS
-                
-            </div>
-
-            <RightSidebar
-                user={loggedIn}
-                transactions={[accounts?.transactions]}
-                banks={accountsData?.slice(0 ,2)} 
-            />
-        </section>
-    )
+      <RightSidebar 
+        user={loggedIn}
+        transactions={account?.transactions}
+        banks={accountsData?.slice(0, 2)}
+      />
+    </section>
+  )
 }
 
 export default Home
